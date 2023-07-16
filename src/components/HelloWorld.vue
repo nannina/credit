cea4128d0a27fa05573b6d6e3129b11a0cb4ff95
zhengn <zhengn@augurit.com>
2023-07-16 10:33:41 +0800

    初始化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,51 +1,58 @@
 <template>
-  <div>
-    <div id="viewDiv" class="clearUl"></div>
-    <div id="clear"></div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
   </div>
 </template>
+
 <script>
-import Map from "@arcgis/core/Map";
-import SceneView from "@arcgis/core/views/SceneView";
-import Layer from "../sdk/Layer";
 export default {
-  name: "HellowWorld",
-  data() {
-    return {
-      map: null,
-      mapView: null,
-    };
-  },
-  methods: {
-    initMap() {
-      var map = new Map({
-        basemap: "hybrid",
-      });
-      var mapView = new SceneView({
-        map: map,
-        container: "viewDiv",
-        camera: {
-          position: {
-            x: -118.808,
-            y: 33.961,
-            z: 2000,
-          },
-          tilt: 75,
-        },
-      });
-    },
-  },
-  mounted() {
-    this.initMap();
-  },
-};
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import "https://js.arcgis.com/4.25/@arcgis/core/assets/esri/themes/dark/main.css";
-#viewDiv {
-  height: 100vh;
-  width: 100%;
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
-
--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,7 @@ import Vue from 'vue'
 import {createRouter, createWebHistory} from 'vue-router'
  
 
-// import index from '@/components/home/index'
 import HelloWorld from '@/components/HelloWorld'
-import buildingSceneLayer from '@/components/Layers/buildingSceneLayer' //建筑安全巡查 鉴定
 
 const routerHistory = createWebHistory()
 const router = createRouter({
@@ -17,11 +15,6 @@ const router = createRouter({
             name: '首页',
             component: HelloWorld,
         },
-        {
-            path: '/buildingSceneLayer',
-            name: 'buildingSceneLayer',
-            component: buildingSceneLayer,
-        }
     ]
 })
 
